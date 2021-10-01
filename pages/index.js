eaf04acdb7b35d130bb504ce0f4d01e7f7701ae0
Thanasan Kumdee <6200313@kvis.ac.th>
2021-10-02 03:40:40 +0700

    um

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,88 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import * as faceapi from "face-api.js";
 
 export default function Home() {
+  const [leftEyePos, setLeftEyePos] = useState(0);
+  const [posX, setPosX] = useState(0);
+  const [loadedModel, setLoadedModel] = useState(false);
+  useEffect(async () => {
+    clearInterval();
+    const video = document.getElementById("myVid");
+    const text = document.getElementById("myText");
+    if (
+      "mediaDevices" in navigator &&
+      "getUserMedia" in navigator.mediaDevices
+    ) {
+      video.setAttribute("playsinline", "");
+      video.setAttribute("autoplay", "");
+      video.setAttribute("muted", "");
+      video.style.width = "200px";
+      video.style.height = "200px";
+
+      let facingMode = "user"; // Can be 'user' or 'environment' to access back or front camera (NEAT!)
+      let constraints = {
+        audio: false,
+        video: {
+          facingMode: facingMode,
+        },
+      };
+      navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function success(stream) {
+          video.srcObject = stream;
+        });
+    }
+
+    const MODEL_URL = "/models";
+
+    await faceapi.loadSsdMobilenetv1Model(MODEL_URL);
+    await faceapi.loadFaceLandmarkModel(MODEL_URL);
+    await faceapi.loadFaceRecognitionModel(MODEL_URL);
+
+    setLoadedModel(true);
+
+    setInterval(async () => {
+      let faceDetection = await faceapi
+        .detectSingleFace(video)
+        .withFaceLandmarks();
+
+      if (faceDetection != undefined) {
+        let currentNosePos = Math.floor(faceDetection.landmarks.getNose()[0].x);
+        console.log(currentNosePos);
+
+        text.style.marginLeft = currentNosePos - 400 + "px";
+
+        // console.log(currentLeftEyePos);
+        // if (leftEyePos == 0) {
+        //   setLeftEyePos(currentLeftEyePos);
+        // } else {
+        //   let leftEyePosDif = currentLeftEyePos - leftEyePos;
+        //   setLeftEyePos(currentLeftEyePos);
+        //   setPosX(posX + leftEyePosDif);
+        //   text.style.marginLeft = posX + "px";
+        // }
+      }
+    }, 750);
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Wholdstill</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1 className="text-6xl font-bold">Wholdstill</h1>
+        <h1 className="text-3xl font-thin mt-4">whole + hold + still</h1>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      {loadedModel ? <h2>Loaded Model</h2> : <h2>Loading Model</h2>}
+      <h1 id="myText" className="transition-all">
+        {" "}
+        DEMO TEXT DEMO TEXT
+      </h1>
+      <video crossorigin="anonymous" id="myVid" />
     </div>
-  )
+  );
 }
