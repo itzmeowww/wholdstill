e460347a06eb03e4c6242c9ee5ed0964d4916f23
Thanasan Kumdee <6200313@kvis.ac.th>
2021-10-02 15:14:16 +0700

    ww

--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,7 @@ export default function Home() {
       video.setAttribute("playsinline", "");
       video.setAttribute("autoplay", "");
       video.setAttribute("muted", "");
-      video.style.width = "200px";
-      video.style.height = "200px";
+      video.style.width = "100px";
 
       let facingMode = "user"; // Can be 'user' or 'environment' to access back or front camera (NEAT!)
       let constraints = {
@@ -37,33 +36,42 @@ export default function Home() {
     await faceapi.loadTinyFaceDetectorModel(MODEL_URL);
 
     setLoadedModel(true);
-    let count = 1;
+    // let count = 1;
     let posX = -1;
+
+    let inputSize = 160;
+    let scoreThreshold = 0.3;
+    const OPTION = new faceapi.TinyFaceDetectorOptions({
+      inputSize,
+      scoreThreshold,
+    });
+
+    let complete = true;
     setInterval(async () => {
       // tiny_face_detector options
-      console.log(`start : ${count}`);
-      let inputSize = 512;
-      let scoreThreshold = 0.3;
-      const OPTION = new faceapi.TinyFaceDetectorOptions({
-        inputSize,
-        scoreThreshold,
-      });
+      // console.log(`start : ${count}`);
 
       // detect all faces and generate full description from image
       // including landmark and descriptor of each face
-      let fullDesc = await faceapi.detectSingleFace(video, OPTION);
-      // console.log(fullDesc);
+      if (complete) {
+        complete = false;
+        let fullDesc = await faceapi.detectSingleFace(video, OPTION);
+        // console.log(fullDesc);
 
-      if (fullDesc != undefined) {
-        let currentX = fullDesc.box.x;
-        if (posX == -1) {
-          posX = currentX;
+        if (fullDesc != undefined) {
+          let currentX = fullDesc.box.x;
+          if (posX == -1) {
+            posX = currentX;
+          }
+          text.style.marginLeft = speedConst * (posX - currentX) + "px";
+          console.log(text.style.marginLeft);
         }
-        text.style.marginLeft = speedConst * (posX - currentX) + "px";
+        complete = true;
       }
-      console.log(`end : ${count}`);
-      count++;
-    }, 300);
+
+      // console.log(`end : ${count}`);
+      // count++;
+    }, 100);
   }, []);
 
   const handleChange = (e) => {
@@ -84,7 +92,7 @@ export default function Home() {
       </main>
       {loadedModel ? <h2>Loaded Model</h2> : <h2>Loading Model</h2>}
       <div className="w-screen max-w-full overflow-hidden flex flex-row justify-center">
-        <h1 id="myText" className="transition-all absolute">
+        <h1 id="myText" className="transition-all">
           {" "}
           DEMO TEXT DEMO TEXT
         </h1>
