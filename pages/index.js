fe82bd2137a8a392cfe2bbe4ed52db3086e25e64
Thanasan Kumdee <6200313@kvis.ac.th>
2021-10-27 16:27:36 +0700

    PWA

--- pages/index.js
+++ pages/index.js
@@ -149,26 +149,103 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-start pt-2 ">
       <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+
         <title>Wholdstill</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icons/icon-256.png" />
+
+        <meta name="application-name" content="Wholdstill" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Wholdstill" />
+        <meta name="description" content="Wholdstill" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/icons/icon.png" />
+        {/* <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+         */}
+        {/*
+        
+        
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/touch-icon-iphone-retina.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="167x167"
+          href="/icons/touch-icon-ipad-retina.png"
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        
+        <link
+          rel="mask-icon"
+          href="/icons/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
+
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://yourdomain.com" />
+        <meta name="twitter:title" content="PWA App" />
+        <meta name="twitter:description" content="Best PWA App in the world" />
+        <meta
+          name="twitter:image"
+          content="https://yourdomain.com/icons/android-chrome-192x192.png"
+        />
+        <meta name="twitter:creator" content="@DavidWShadow" />
+
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="PWA App" />
+        <meta property="og:description" content="Best PWA App in the world" />
+        <meta property="og:site_name" content="PWA App" />
+        <meta property="og:url" content="https://yourdomain.com" />
+        <meta
+          property="og:image"
+          content="https://yourdomain.com/icons/apple-touch-icon.png"
+        /> */}
       </Head>
-      <div className="text-center mb-10 flex flex-col justify-start">
-        <h1 className="text-7xl font-bold">Wholdstill</h1>
+      <div className="text-center mb-10 flex flex-col justify-start items-center">
+        <h1 className="text-6xl font-bold">Wholdstill</h1>
         <h1 className="text-3xl font-thin">whole + hold + still</h1>
-        <button
-          className="bg-blue-400 rounded-md px-2 py-1 shadow-lg my-2"
-          onClick={toggleSetting}
-        >
-          Setting
-        </button>
 
         <div>
-          <input id="image-file" type="file" />
+          <button
+            className="bg-blue-400 rounded-md px-2 py-1 shadow-lg my-2 w-24"
+            onClick={toggleSetting}
+          >
+            Setting
+          </button>
+        </div>
+        <div>
+          <input id="image-file" type="file" className=" w-1/2" />
           <button
             className="bg-blue-400 rounded-md px-2 py-1 shadow-lg "
             onClick={updateImg}
           >
-            update
+            Update Image
           </button>
         </div>
       </div>
